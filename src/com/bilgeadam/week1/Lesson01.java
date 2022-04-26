b36b322e8ab154dfe96371e8ef1a8fcd7d0254be
cagriturkmen <c.turkmen2@hotmail.com>
2022-04-26 13:54:37 +0300

    first commit

---
+++ src/com/bilgeadam/week1/Lesson01.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.week1;
+
+public class Lesson01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/bilgeadam/week2/Lesson10.java
@@ -0,0 +1,10 @@
+package com.bilgeadam.week2;
+
+public class Lesson10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
